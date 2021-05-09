d6412001b7542dacbf22b56e2ba7394bd5d0ec6e
williambayliss <wmbayliss@gmail.com>
2021-05-09 16:59:09 -0400

    Added scope past and upcomming to Events, and class methods that accomplish same thing but are commented out, just in case

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :guest_lists
     has_many :attendees, through: :guest_lists, source: :attendee
+
+    scope :past, -> { where("date < ?", Date.today) }
+    scope :upcoming, -> { where("date > ?", Date.today) }
+
+    # def self.past
+    #     self.where("date < ?", Date.today)
+    # end
+
+    # def self.upcoming
+    #     self.where("date > ?", Date.today)
+    # end
 end
